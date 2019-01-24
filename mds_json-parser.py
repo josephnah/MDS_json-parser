43181353c7653507ca1ed4b4f445bf973346bf26
Joseph Nah <josephcnah@gmail.com>
2019-01-23 19:52:12 -0500

    work in progress

--- mds_json-parser.py
+++ mds_json-parser.py
@@ -4,6 +4,10 @@
 
 import glob, getpass
 import json
+import scipy
+
+# Array of stimuli list (to compare with stimuli list in JSON file)
+stim_array = [10,11,1,20,21,2,30,31,3,41,40,4,50,51,5]
 
 # Define path to data
 userName = getpass.getuser()
@@ -13,9 +17,16 @@ data_files = glob.glob(data_dir)
 participants = 1 # len(data_files)
 
 for file in range(participants):
+    # convert string to object via json.loads
     json_data = json.loads(open(data_files[file]).read())
-    # print(json_data)
 
     # get participant number (this might vary based on your tasks)
-    instructions = json_data["tasks"][0]
-    parNo = instructions["Participant Number"]
+    parNo = json_data["tasks"][0]["Participant Number"]
+
+    # get stimuli list (confirm order of stim to understand RDM)
+    stim_compare = []
+    stimuli = json_data["tasks"][2]["stimuli"]
+    for stim in range (len(stimuli)):
+        stim_compare.append(int(stimuli[stim]["name"]))
+    print(stim_compare==stim_array)
+    print((json_data["tasks"][2]["rdm"]))
